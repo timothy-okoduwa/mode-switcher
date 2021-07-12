ae5365f92c4189ce3d5382da509f3ce91385f387
unknown <timmyleeokoduwa7@gmail.com>
2021-07-12 13:04:33 +0100

    mode

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "antd/dist/antd.css";
+import { Button } from "antd";
+import React, { useState } from "react";
+import a from "./t.JPG";
+import style from "styled-components";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
+import storage from "local-storage-fallback";
+const GlobalStyled = createGlobalStyle`
+body{
+  background-color: ${({ theme }) =>
+    theme.myTheme === "dark" ? "black" : "white"};
+  color: ${({ theme }) => (theme.myTheme === "dark" ? "white" : "black")}
+
+}
+`;
 
 function App() {
+  const storeThemeChoice = () => {
+    const saveTheme = storage.getItem("toggle");
+    return saveTheme ? JSON.parse(saveTheme) : { myTheme: "light" };
+  };
+  const [toggle, setToggle] = useState(storeThemeChoice);
+  React.useEffect(() => {
+    storage.setItem("toggle", JSON.stringify(toggle));
+  }, [toggle]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ThemeProvider theme={toggle}>
+      <GlobalStyled />
+      <Container>
+        <Holder>this is my name</Holder>
+        <Button
+          type="primary"
+          onClick={() => {
+            setToggle(
+              toggle.myTheme === "dark"
+                ? {
+                    myTheme: "light",
+                  }
+                : { myTheme: "dark" }
+            );
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          ToggletMode
+        </Button>
+      </Container>
+    </ThemeProvider>
   );
 }
 
 export default App;
+const Container = style.div`
+height:100vh;
+width:100vw;
+display:flex;
+align-items:center;
+justify-content:center;
+flex-direction:column;
+
+`;
+const Holder = style.div`
+font-size:90px;
+
+`;
